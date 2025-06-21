94cf6a82021edb3c533e04edc24bebd03c756fee
CoreMaker <a845656974@outlook.com>
2025-06-22 02:45:49 +0800

    UPDATE

---
+++ RA4M2_IOT_Project10/Debug/src/lps22df_reg.d
@@ -0,0 +1,2 @@
+src/lps22df_reg.o: ../src/lps22df_reg.c ../src/lps22df_reg.h
+../src/lps22df_reg.h:
---
+++ RA4M2_IOT_Project11/ra_gen/vector_data.h
@@ -0,0 +1,54 @@
+/* generated vector header file - do not edit */
+#ifndef VECTOR_DATA_H
+#define VECTOR_DATA_H
+#ifdef __cplusplus
+        extern "C" {
+        #endif
+/* Number of interrupts allocated */
+#ifndef VECTOR_DATA_IRQ_COUNT
+#define VECTOR_DATA_IRQ_COUNT    (13)
+#endif
+/* ISR prototypes */
+void sci_uart_rxi_isr(void);
+void sci_uart_txi_isr(void);
+void sci_uart_tei_isr(void);
+void sci_uart_eri_isr(void);
+void sci_i2c_txi_isr(void);
+void sci_i2c_tei_isr(void);
+void r_icu_isr(void);
+void adc_scan_end_isr(void);
+
+/* Vector table allocations */
+#define VECTOR_NUMBER_SCI9_RXI ((IRQn_Type) 0) /* SCI9 RXI (Receive data full) */
+#define SCI9_RXI_IRQn          ((IRQn_Type) 0) /* SCI9 RXI (Receive data full) */
+#define VECTOR_NUMBER_SCI9_TXI ((IRQn_Type) 1) /* SCI9 TXI (Transmit data empty) */
+#define SCI9_TXI_IRQn          ((IRQn_Type) 1) /* SCI9 TXI (Transmit data empty) */
+#define VECTOR_NUMBER_SCI9_TEI ((IRQn_Type) 2) /* SCI9 TEI (Transmit end) */
+#define SCI9_TEI_IRQn          ((IRQn_Type) 2) /* SCI9 TEI (Transmit end) */
+#define VECTOR_NUMBER_SCI9_ERI ((IRQn_Type) 3) /* SCI9 ERI (Receive error) */
+#define SCI9_ERI_IRQn          ((IRQn_Type) 3) /* SCI9 ERI (Receive error) */
+#define VECTOR_NUMBER_SCI2_TXI ((IRQn_Type) 4) /* SCI2 TXI (Transmit data empty) */
+#define SCI2_TXI_IRQn          ((IRQn_Type) 4) /* SCI2 TXI (Transmit data empty) */
+#define VECTOR_NUMBER_SCI2_TEI ((IRQn_Type) 5) /* SCI2 TEI (Transmit end) */
+#define SCI2_TEI_IRQn          ((IRQn_Type) 5) /* SCI2 TEI (Transmit end) */
+#define VECTOR_NUMBER_ICU_IRQ6 ((IRQn_Type) 6) /* ICU IRQ6 (External pin interrupt 6) */
+#define ICU_IRQ6_IRQn          ((IRQn_Type) 6) /* ICU IRQ6 (External pin interrupt 6) */
+#define VECTOR_NUMBER_SCI0_RXI ((IRQn_Type) 7) /* SCI0 RXI (Receive data full) */
+#define SCI0_RXI_IRQn          ((IRQn_Type) 7) /* SCI0 RXI (Receive data full) */
+#define VECTOR_NUMBER_SCI0_TXI ((IRQn_Type) 8) /* SCI0 TXI (Transmit data empty) */
+#define SCI0_TXI_IRQn          ((IRQn_Type) 8) /* SCI0 TXI (Transmit data empty) */
+#define VECTOR_NUMBER_SCI0_TEI ((IRQn_Type) 9) /* SCI0 TEI (Transmit end) */
+#define SCI0_TEI_IRQn          ((IRQn_Type) 9) /* SCI0 TEI (Transmit end) */
+#define VECTOR_NUMBER_SCI0_ERI ((IRQn_Type) 10) /* SCI0 ERI (Receive error) */
+#define SCI0_ERI_IRQn          ((IRQn_Type) 10) /* SCI0 ERI (Receive error) */
+#define VECTOR_NUMBER_ICU_IRQ4 ((IRQn_Type) 11) /* ICU IRQ4 (External pin interrupt 4) */
+#define ICU_IRQ4_IRQn          ((IRQn_Type) 11) /* ICU IRQ4 (External pin interrupt 4) */
+#define VECTOR_NUMBER_ADC0_SCAN_END ((IRQn_Type) 12) /* ADC0 SCAN END (End of A/D scanning operation) */
+#define ADC0_SCAN_END_IRQn          ((IRQn_Type) 12) /* ADC0 SCAN END (End of A/D scanning operation) */
+/* The number of entries required for the ICU vector table. */
+#define BSP_ICU_VECTOR_NUM_ENTRIES (13)
+
+#ifdef __cplusplus
+        }
+        #endif
+#endif /* VECTOR_DATA_H */
